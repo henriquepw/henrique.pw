ea3dc9ac510adc06bf3a500c075504d5d1991405
Henrique Martins <enrque.ns@gmail.com>
2019-02-24 09:20:02 -0300

    Updates

---
+++ precache-manifest.8508eb2323c68f1584786ccfb48e48d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0eaa6fb357c2103fb16496fcfe02200",
+    "url": "/portfolio/static/media/photo.e0eaa6fb.png"
+  },
+  {
+    "revision": "8af9aef43121963c5191d7cf5f0f17b4",
+    "url": "/portfolio/static/media/background2.8af9aef4.png"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "c6af51a696de267b1d99",
+    "url": "/portfolio/static/js/main.c6af51a6.chunk.js"
+  },
+  {
+    "revision": "b6c201147d832d38c1b4",
+    "url": "/portfolio/static/js/2.b6c20114.chunk.js"
+  },
+  {
+    "revision": "a8ef48196b2a4db9f36e3123a7ec8b62",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.646ae3e3ebbb945ce4ffeccffe7d8d0b.js"
+  "/portfolio/precache-manifest.8508eb2323c68f1584786ccfb48e48d3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
