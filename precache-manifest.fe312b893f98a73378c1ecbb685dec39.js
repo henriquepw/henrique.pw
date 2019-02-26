a3c256ccded251adcc8f7cffbfc2d02e068f7040
Henrique Martins <enrque.ns@gmail.com>
2019-02-26 20:28:12 -0300

    Updates

---
+++ precache-manifest.fe312b893f98a73378c1ecbb685dec39.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "615512ce496e5b319eb3",
+    "url": "/static/js/main.615512ce.chunk.js"
+  },
+  {
+    "revision": "8af9aef43121963c5191d7cf5f0f17b4",
+    "url": "/static/media/background2.8af9aef4.png"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "e0eaa6fb357c2103fb16496fcfe02200",
+    "url": "/static/media/photo.e0eaa6fb.png"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "5a4b828809b847f56f51",
+    "url": "/static/js/2.5a4b8288.chunk.js"
+  },
+  {
+    "revision": "5a4b828809b847f56f51",
+    "url": "/static/css/2.4e1fc4a9.chunk.css"
+  },
+  {
+    "revision": "26aa503eeaf8fd68d7eac718502d1600",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.00ab0fc21a6903078e8f9bcc96a15827.js"
+  "/precache-manifest.fe312b893f98a73378c1ecbb685dec39.js"
 );
 
 workbox.clientsClaim();
