69bc31205c8f99577146d2a93f288a9de8f41f3c
Henrique Martins <enrque.ns@gmail.com>
2019-07-12 14:10:43 -0300

    Updates

---
+++ precache-manifest.1c05c833ba6908f2e275800d68172205.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d0d6f04465a756b3f59f3af5cbfbf54",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b495572b4314b1efec11",
+    "url": "/portfolio/static/js/2.f933cad5.chunk.js"
+  },
+  {
+    "revision": "485658f22e72c6388e6a",
+    "url": "/portfolio/static/js/main.e7014b30.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "8af9aef43121963c5191d7cf5f0f17b4",
+    "url": "/portfolio/static/media/background2.8af9aef4.png"
+  },
+  {
+    "revision": "d2d18d5d93b75df8410cc66855704717",
+    "url": "/portfolio/static/media/factorio.d2d18d5d.jpg"
+  },
+  {
+    "revision": "e0eaa6fb357c2103fb16496fcfe02200",
+    "url": "/portfolio/static/media/photo.e0eaa6fb.png"
+  },
+  {
+    "revision": "090352369a9fb8b59153a8a8ee0a95ef",
+    "url": "/portfolio/static/media/the-talos-principle.09035236.jpg"
+  },
+  {
+    "revision": "ffa9151d56bc5f8f7a8ffd21cc9838ec",
+    "url": "/portfolio/static/media/witcher.ffa9151d.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.ff1e638ad6b15aaffd679467edf86703.js"
+  "/portfolio/precache-manifest.1c05c833ba6908f2e275800d68172205.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
