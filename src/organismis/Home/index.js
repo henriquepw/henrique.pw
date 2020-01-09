39b85611537a74f69b0f94d7fa62de40e486dc1d
Henrique Miranda <enrque.ns@gmail.com>
2020-01-09 17:46:24 -0300

    :lipstick: style: parallax home

--- src/organismis/Home/index.js
+++ src/organismis/Home/index.js
@@ -1,28 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+import { motion, useMotionValue, useTransform } from 'framer-motion';
 import PropTypes from 'prop-types';
 
 import SocialList from '~/molecules/SocialList';
 
 import { Container, ProfileImg, Divider } from './styles';
 
+const threshold = Array.from(new Array(50), (_, index) => (index + 1) / 50);
+
 function Home({ forwardRef, pageHeight }) {
+  const opacity = useMotionValue(1);
+  const contentY = useTransform(opacity, value => -((1 / value - 1) * 80));
+
   function handleNextSection(e) {
     e.preventDefault();
     document.getElementById('education').scrollIntoView();
   }
 
+  useEffect(() => {
+    function parallaxContent([{ intersectionRatio }]) {
+      opacity.set(intersectionRatio >= 0.9 ? 1 : intersectionRatio);
+    }
+
+    const observer = new IntersectionObserver(parallaxContent, {
+      threshold,
+    });
+
+    if (forwardRef.current) {
+      observer.observe(forwardRef.current);
+    }
+
+    return () => observer.disconnect();
+  }, [forwardRef, opacity, pageHeight, contentY]);
+
   return (
-    <Container id="home" ref={forwardRef} height={pageHeight}>
-      <ProfileImg />
-      <h1>
-        Henrique <strong>Miranda</strong>
-      </h1>
-      <h2>
-        Telematics student, Full Stack Developer <strong>&</strong> Designer
-      </h2>
-      <Divider />
-      <SocialList />
+    <Container id="home" ref={forwardRef} /*  height={pageHeight} */>
+      <motion.div style={{ opacity, y: contentY, width: '100%' }}>
+        <ProfileImg />
+        <h1>
+          Henrique <strong>Miranda</strong>
+        </h1>
+        <h2>
+          Telematics student, Full Stack Developer <strong>&</strong> Designer
+        </h2>
+        <Divider />
+        <SocialList />
+      </motion.div>
       <a
         href="#education"
         alt="next section"
