c2858ee1c68a93d4f58c068ca711257091a159c3
Henrique Miranda <enrque.ns@gmail.com>
2020-12-29 11:40:16 -0300

    :hammer: refactor: contentful api

--- src/services/contentful.ts
+++ src/services/contentful.ts
@@ -1,8 +1,8 @@
 import { createClient } from 'contentful';
 
-const client = createClient({
+const contentfulApi = createClient({
   accessToken: process.env.NEXT_CONTENTUL_ACESS_TOKEN,
   space: process.env.NEXT_CONTENTUL_SPACE_ID,
 });
 
-export default client;
+export default contentfulApi;
--- src/services/spotify.ts
+++ src/services/spotify.ts
@@ -1,34 +1,56 @@
 import axios from 'axios';
 
+import tryGet from '@/utils/tryGet';
+
+import { Track } from '@/interfaces/track';
+
 const clientId = process.env.NEXT_SPOTIFY_CLIENT_ID;
 const clientSecret = process.env.NEXT_SPOTIFY_CLIENT_SECRET;
-
-export async function getSpotifyToken(): Promise<string> {
-  try {
-    const credentials = Buffer.from(`${clientId}:${clientSecret}`);
-
-    const response = await axios('https://accounts.spotify.com/api/token', {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization: `Basic ${credentials.toString('base64')}`,
-      },
-      data: 'grant_type=client_credentials',
-      method: 'POST',
-    });
-
-    const accessToken: string = response.data.access_token;
-    const tokenType: string = response.data.token_type;
-
-    axios.defaults.headers.Authorization = `${tokenType} ${accessToken}`;
-
-    return `${tokenType} ${accessToken}`;
-  } catch (error) {
-    console.warn(error);
-  }
-
-  return '';
-}
 
 export const spotifyApi = axios.create({
   baseURL: 'https://api.spotify.com/v1/',
 });
+
+export async function getSpotifyToken(): Promise<string> {
+  const credentials = Buffer.from(`${clientId}:${clientSecret}`);
+
+  const response = await axios('https://accounts.spotify.com/api/token', {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${credentials.toString('base64')}`,
+    },
+    data: 'grant_type=client_credentials',
+    method: 'POST',
+  });
+
+  const accessToken: string = response.data.access_token;
+  const tokenType: string = response.data.token_type;
+
+  axios.defaults.headers.Authorization = `${tokenType} ${accessToken}`;
+
+  return `${tokenType} ${accessToken}`;
+}
+
+export async function getSpotifyPlaylist(): Promise<Track[]> {
+  await tryGet(getSpotifyToken());
+
+  const [response, error] = await tryGet(
+    spotifyApi.get(`playlists/${process.env.NEXT_SPOTIFY_PLAYLIST_ID}`),
+  );
+
+  if (error) return [];
+
+  const tracks: Track[] = response.data.tracks.items.map(({ track }) => ({
+    id: track.id,
+    name: track.name,
+    previewUrl: track.preview_url,
+    externalUrl: track.external_urls.spotify,
+    artists: track.artists,
+    album: {
+      image: track.album.images[0],
+      externalUrl: track.album.external_urls.spotify,
+    },
+  }));
+
+  return tracks.filter((track) => track.previewUrl);
+}
